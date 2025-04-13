80375c25a89f909530e5fae42c557acde90e2309
Vojtěch Wilhelm Skyba <skymmel05@gmail.com>
2025-04-13 23:01:20 +0200

    základ

---
+++ src/module/Header.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Header() {
+    const [navOpen, setNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        setNavOpen(!navOpen);
+    };
+
+    const closeNav = () => {
+        setNavOpen(false);
+    };
+
+    return (
+        <header className="pageHeader">
+            <div className="content">
+                <div className="mainHeader">
+                    <Link href="/">
+                        <Image src="/logo.svg" alt="Machovci" width={36} height={36}/>
+                    </Link>
+                    <button onClick={toggleNav}>
+                        <Image src="/nav.svg" alt="Menu" width={20} height={20} />
+                    </button>
+                </div>
+                <nav id="nav" className={navOpen ? "open" : ""}>
+                    <ul>
+                        <li>
+                            <Link href="/" onClick={closeNav}>Úvod</Link>
+                        </li>
+                        <li>
+                            <Link href="/#portfolio" onClick={closeNav}>Portfolio</Link>
+                        </li>
+                        <li>
+                            <Link href="/kontakt" onClick={closeNav}>Kontakt</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
